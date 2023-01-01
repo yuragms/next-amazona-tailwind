d507bd725f5d18add4563f8f2ffec84820498d73
yuragms <yura.gms@gmail.com>
2023-01-01 23:48:58 +0200

    ProductItem8

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -11,7 +11,16 @@ export default function ProductItem({ product }) {
           className="rounded shadow"
         />
       </Link>
-      <div className="flex "></div>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`}>
+          <h2 className="text-lg">{product.name}</h2>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 }
