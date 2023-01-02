43eb0bac977ff6cdcff0d312b0ad55588bc9b6bb
yuragms <yura.gms@gmail.com>
2023-01-02 21:19:07 +0200

    createLoginForm14

--- pages/cart.js
+++ pages/cart.js
@@ -94,7 +94,7 @@ function CartScreen() {
               </li>
               <li>
                 <button
-                  onClick={() => router.push('/shipping')}
+                  onClick={() => router.push('login?redirect=/shipping')}
                   className="primary-button w-full"
                 >
                   Check Out
