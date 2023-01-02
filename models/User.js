622aad831649c4255fae2f13a03c1378e8a104db
yuragms <yura.gms@gmail.com>
2023-01-02 22:52:23 +0200

    mongoDb15

---
+++ models/User.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
