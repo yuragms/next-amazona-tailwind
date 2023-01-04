26eb6471670448edf753f34ffcad482415c7cf5f
yuragms <yura.gms@gmail.com>
2023-01-05 00:12:43 +0200

    tryFix

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
   },
   { timestamps: true }
 );
