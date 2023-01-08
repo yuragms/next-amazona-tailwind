7347ffcada8aa00ab67734eca3ea6cf69ef29e59
yuragms <yura.gms@gmail.com>
2023-01-08 22:06:18 +0200

    placePrderScreen22

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -1,14 +1,62 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
-import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
+import React, { useContext, useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import CheckoutWizard from '../components/CheckoutWizard';
 import Layout from '../components/Layout';
+import { getError } from '../utils/error';
 import { Store } from '../utils/Store';
 
-export default function PlaceOrderScreen() {
+function PlaceOrderScreen() {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
   const { cartItems, shippingAddress, paymentMethod } = cart;
+
+  const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
+
+  const itemsPrice = round2(
+    cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
+  ); // 123.4567 => 123.46
+  const shippingPrice = itemsPrice > 200 ? 0 : 15;
+  const taxPrice = round2(itemsPrice * 0.15);
+  const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!paymentMethod) {
+      router.push('/payment');
+    }
+  }, [paymentMethod, router]);
+
+  const [loading, setLoading] = useState(false);
+
+  const placeOrderHandler = async () => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post('/api/orders', {
+        orderItems: cartItems,
+        shippingAddress,
+        paymentMethod,
+        shippingPrice,
+        itemsPrice,
+        taxPrice,
+        totalPrice,
+      });
+      setLoading(false);
+      dispatch({ type: 'CART_CLEAR_ITEMS' });
+      Cookies.set('cart', JSON.stringify({ ...cart, cartItems: [] }));
+      router.push(`/order/${data._id}`);
+    } catch (err) {
+      setLoading(false);
+      toast.error(getError(err));
+    }
+  };
+
   return (
     <Layout title="Place Order">
       <CheckoutWizard activeStep={3} />
@@ -40,13 +88,13 @@ export default function PlaceOrderScreen() {
             </div>
             <div className="card overflow-x-auto p-5">
               <h2 className="mb-2 text-lg">Order Items</h2>
-              <table className="min-wfull">
+              <table className="min-w-full">
                 <thead className="border-b">
                   <tr>
                     <th className="px-5 text-left">Item</th>
                     <th className="p-5 text-right">Quantity</th>
                     <th className="p-5 text-right">Price</th>
-                    <th className="p-5">Action</th>
+                    <th className="p-5 text-right">SubTotal</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -67,14 +115,64 @@ export default function PlaceOrderScreen() {
                           {item.name}
                         </Link>
                       </td>
+                      <td className="p-5 text-right">{item.quantity}</td>
+                      <td className="p-5 text-right">${item.price}</td>
+                      <td className="p-5 text-right">
+                        ${item.quantity * item.price}
+                      </td>
                     </tr>
                   ))}
                 </tbody>
               </table>
+              <div>
+                <Link href="/cart">Edit</Link>
+              </div>
             </div>
+          </div>
+          <div className="card p-5">
+            <h2 className="mb-2 text-lg">Order Summary</h2>
+            <ul>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Items</div>
+                  <div>${itemsPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Tax</div>
+                  <div>${taxPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Shipping</div>
+                  <div>${shippingPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="mb-2 flex justify-between">
+                  <div>Total</div>
+                  <div>${totalPrice}</div>
+                </div>
+              </li>
+              <li>
+                <button
+                  disabled={loading}
+                  onClick={placeOrderHandler}
+                  className="primary-button w-full"
+                >
+                  {loading ? 'Loading...' : 'Place Order'}
+                </button>
+              </li>
+            </ul>
           </div>
         </div>
       )}
     </Layout>
   );
 }
+
+PlaceOrderScreen.auth = true;
+
+export default dynamic(() => Promise.resolve(PlaceOrderScreen), { ssr: false });
