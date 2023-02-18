5ce186e4b56c3e6cda008967ab88809c86632e22
yuragms <yura.gms@gmail.com>
2023-02-18 17:35:53 +0200

    CreateDashboardScreen29

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,6 +1,49 @@
+import axios from 'axios';
 import Link from 'next/link';
-import React, { useReducer } from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import React, { useEffect, useReducer } from 'react';
 import Layout from '../../components/Layout';
+import { getError } from '../../utils/error';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true, error: '' };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, summary: action.payload, error: '' };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
 
 export default function AdminDashboardScreen() {
   const [{ loading, error, summary }, dispatch] = useReducer(reducer, {
@@ -8,6 +51,31 @@ export default function AdminDashboardScreen() {
     summary: { salesData: [] },
     error: '',
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/summary`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const data = {
+    labels: summary.salesData.map((x) => x._id), // 2022/01 2022/03
+    datasets: [
+      {
+        label: 'Sales',
+        backgroundColor: 'rgba(162,222,208,1)',
+        data: summary.salesData.map((x) => x.totalSales),
+      },
+    ],
+  };
+
   return (
     <Layout title="Admin Dashboard">
       <div className="grid md:grid-cols-4 md:gap-5">
@@ -39,7 +107,7 @@ export default function AdminDashboardScreen() {
             <div>
               <div className="grid grid-cols-1 md:grid-cols-4">
                 <div className="card m-5 p-5">
-                  <p className="text-3xl">{summary.ordersPrice}</p>
+                  <p className="text-3xl">${summary.ordersPrice}</p>
                   <p>Sales</p>
                   <Link href="/admin/orders">View sales</Link>
                 </div>
@@ -59,6 +127,13 @@ export default function AdminDashboardScreen() {
                   <Link href="/admin/products">View users</Link>
                 </div>
               </div>
+              <h2 className="text-xl">Sales Report</h2>
+              <Bar
+                options={{
+                  legend: { display: true, position: 'right' },
+                }}
+                data={data}
+              />
             </div>
           )}
         </div>
