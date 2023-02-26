d4c1ff8205e60eb4db92964a5a3fd1429eb83a7f
yuragms <yura.gms@gmail.com>
2023-02-26 21:47:20 +0200

    ListUsersDeleteUsers36

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import Link from 'next/link';
 import React, { useEffect, useReducer } from 'react';
+import { toast } from 'react-toastify';
+import Layout from '../../components/Layout';
 import { getError } from '../../utils/error';
 
 function reducer(state, action) {
@@ -7,7 +10,7 @@ function reducer(state, action) {
     case 'FETCH_REQUEST':
       return { ...state, loading: true, error: '' };
     case 'FETCH_SUCCESS':
-      return { ...state, loading: false, products: action.payload, error: '' };
+      return { ...state, loading: false, users: action.payload, error: '' };
     case 'FETCH_FAIL':
       return { ...state, loading: false, error: action.payload };
 
@@ -29,28 +32,119 @@ function AdminUsersScreen() {
   const [{ loading, error, users, successDelete, loadingDelete }, dispatch] =
     useReducer(reducer, {
       loading: true,
-      products: [],
+      users: [],
       error: '',
     });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                dispatch({ type: 'FETCH_REQUEST' });
-                const { data } = await axios.get(`/api/admin/users`);
-                dispatch({type: 'FETCH_SUCCESS', payload: data}) 
-            } catch (err) {
-                dispatch({ type: 'FETCH_FAIL', payload: getError(err) }); 
-            }
-            if (successDelete) {
-                dispatch({type: 'DELETE_RESET'}) 
-            } else {
-                fetchData();
-            }
-        }
-        },[successDelete]
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get(`/api/admin/users`);
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
 
-  return <div>users</div>;
+    if (successDelete) {
+      dispatch({ type: 'DELETE_RESET' });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
+
+  const deleteHandler = async (userId) => {
+    if (!window.confirm('Are you sure')) {
+      return;
+    }
+    try {
+      dispatch({ type: 'DELETE_REQUEST' });
+      await axios.delete(`/api/admin/users/${userId}`);
+      dispatch({ type: 'DELETE_SUCCESS' });
+      toast.success('User deleted successfully');
+    } catch (err) {
+      dispatch({ type: 'DELETE_FAIL' });
+      toast.error(getError(err));
+    }
+  };
+
+  return (
+    <Layout>
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div>
+          <ul>
+            <li>
+              <Link href="/admin/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">Orders</Link>
+            </li>
+            <li>
+              <Link href="/admin/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/admin/users" className="font-bold">
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="overflow-x-auto md:col-span-3">
+          <h1 className="mb-4 text-xl">Users</h1>
+          {loadingDelete && <div>Deleting...</div>}
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-left">ID</th>
+                    <th className="p-5 text-left">NAME</th>
+                    <th className="p-5 text-left">EMAIL</th>
+                    <th className="p-5 text-left">ADMIN</th>
+                    <th className="p-5 text-left">ACTIONS</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {users.map((user) => (
+                    <tr key={user._id} className="border-b">
+                      <td className="p-5">{user._id.substring(20, 24)}</td>
+                      <td className="p-5">{user.name}</td>
+                      <td className="p-5">{user.email}</td>
+                      <td className="p-5">{user.isAdmin ? 'YES' : 'NO'}</td>
+                      <td className="p-5">
+                        <Link
+                          href={`/admin/user/${user._id}`}
+                          passHref
+                          className="default-button"
+                          type="button"
+                        >
+                          Edit
+                        </Link>
+                        &nbsp;
+                        <button
+                          type="button"
+                          className="default-button"
+                          onClick={() => deleteHandler(user._id)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 AdminUsersScreen.auth = { adminOnly: true };
