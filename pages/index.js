15184f1d36a6146fba28365c8ff9dd462da0ffae
yuragms <yura.gms@gmail.com>
2023-02-27 16:42:34 +0200

    addCarouselSlidershow37

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,11 @@ import { Store } from '../utils/Store';
 import { useContext } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import Link from 'next/link';
 
-export default function Home({ products }) {
+export default function Home({ products, featuredProducts }) {
   const { state, dispatch } = useContext(Store);
   const { cart } = state;
 
@@ -27,6 +30,16 @@ export default function Home({ products }) {
   };
   return (
     <Layout title="Home page">
+      <Carousel showThumbs={false} autoPlay>
+        {featuredProducts.map((product) => (
+          <div key={product._id}>
+            <Link href={`/product/${product.slug}`} className="flex">
+              <img src={product.banner} alt={product.name} />
+            </Link>
+          </div>
+        ))}
+      </Carousel>
+      <h2 className="h2 my-4">Latest Products</h2>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product) => (
           <ProductItem
@@ -43,8 +56,10 @@ export default function Home({ products }) {
 export async function getServerSideProps() {
   await db.connect();
   const products = await Product.find().lean();
+  const featuredProducts = await Product.find({ isFeatured: true }).lean();
   return {
     props: {
+      featuredProducts: featuredProducts.map(db.convertDocToObj),
       products: products.map(db.convertDocToObj),
     },
   };
